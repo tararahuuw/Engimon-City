ca99049e76abbb9d9edf91beea424790d1f0b361
thivh <13519085@std.stei.itb.ac.id>
2021-03-19 17:01:20 +0700

    update battle

--- Battle.cpp
+++ Battle.cpp
@@ -4,36 +4,52 @@
 #include "Battle.h"
 using namespace std;
 
-float Battle::table[5][5] = {{1,0,1,0.5,2},{2,1,0,1,1},{1,2,1,0,1.5},{1.5,1,2,1,0},{0,1,0.5,2,1}};
 
 
-Battle::Battle(Engimon& e1,Engimon& e2) {
-    this->e1 = e1;
-    this->e2 = e2;
-}
-Battle::~Battle() {
-    this->e1.~Engimon();
-    this->e2.~Engimon();
-}
-Engimon& Battle::getEngimon1() {
-    return this->e1;
-}
-Engimon& Battle::getEngimon2() {
-    return this->e2;
-}
-void Battle::fight() {
-    float multiplier1 = 0;
-    float multiplier2 = 0;
-    
-    for (auto i = this->e1.getElement().begin(); i != this->e1.getElement().end(); ++i) {
-        for (auto j = this->e2.getElement().begin(); i != this->e2.getElement().end(); ++i) {
-            if(table[*i][*j] > multiplier1) {
-                multiplier1 = table[*i][*j];
+float Battle::advantage(Engimon& e1,Engimon& e2,int n) {
+    int x = 0;
+    int y = 0;
+    for (auto i = e1.getElement().begin(); i != e1.getElement().end(); ++i) {
+        for (auto j = e2.getElement().begin(); i != e2.getElement().end(); ++i) {
+            if(table[*i][*j] > x) {
+                x = table[*i][*j];
             }
-            if(table[*j][*i] > multiplier2) {
-                multiplier2 = table[*j][*i];
+            if(table[*j][*i] > y) {
+                y = table[*j][*i];
             }
         }
     }
+    if(n == 1) {
+        return x;
+    }
+    else if(n == 2) {
+        return y;
+    }
+}
+float Battle::countPower(Engimon& e,float adv) {
+    float total = e.getLevel() * adv;
+    vector<Skill> skill = e.getSkills();
+    for (auto i = skill.begin(); i != skill.end(); ++i) {
+        total = total + (i->getBasePower() * i->getMasteryLevel());
+    }
+    return total;
+
+}
+void Battle::attack(Player& p,Engimon& e1,Engimon& e2) {
+    float adv1 = advantage(e1,e2,1);
+    float adv2 = advantage(e1,e2,2);
+    float power1 = countPower(e1,adv1);
+    float power2 = countPower(e2,adv2);
+    if(power1 >= power2) {
+        int x = rand() % 10 + 1;
+        // e2.setWild(false);
+        e1.addEXP(e2.getLevel()*5);
+        // p.addItem(x)
         
+        
+    }
+    else {
+        e1.setStatus(false);
+    }
+
 }
--- Battle.h
+++ Battle.h
@@ -7,19 +7,18 @@
 #include "Player.h"
 using namespace std;
 
+
+const float table[5][5] = {{1,0,1,0.5,2},{2,1,0,1,1},{1,2,1,0,1.5},{1.5,1,2,1,0},{0,1,0.5,2,1}};
+
 class Battle {
-    private:
-    static float table[5][5];
-    Engimon e1;
-    Engimon e2;
     
     public:
-    Battle();
-    Battle(Engimon& e1, Engimon& e2);
-    ~Battle();
-    Engimon& getEngimon1();
-    Engimon& getEngimon2();
-    void fight();
+    // untuk mengetahui element advantage, n menandakan engimon mana yang ingin diketahui(n = 1 artinya engimon 1)
+    static float advantage(Engimon& e1,Engimon& e2,int n);
+    // inisiasi battle
+    static void attack(Player& p,Engimon& e1,Engimon& e2);
+    // menghitung power sebuah engimon
+    static float countPower(Engimon& e,float adv);
 };
 
 #endif
